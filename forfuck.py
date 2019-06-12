995bab3f0cc74ed4170f44d09050127d8dee3d30
WangJh <wang_jinghong@topsec.com.cn>
2019-06-12 17:37:42 +0800

    haha,imtiancai

---
+++ forfuck.py
@@ -0,0 +1,12 @@
+
+
+#发泄一下
+import random
+
+randint=random.randint(0,100)
+
+for i in range(101):
+    print("fuckyourmom",'zjx','gxl','zz','zyzx')
+    if randint==i :
+        break
+print("fucktime",randint)
