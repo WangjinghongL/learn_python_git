c0fcaee7757ca0b1d2e7789302cc00d0137c36aa
WangJh <15133118844@163.com>
2019-06-25 09:30:42 +0800

    自定义方法

--- learn/trim.py
+++ learn/trim.py
@@ -1,5 +1,5 @@
 def trim(s):
-    if(len(s)!=0):
+    if len(s)!=0:
        while s[:1]==' ':
            s=s[1:]
        while s[-1:]==' ':
@@ -9,8 +9,19 @@ def trim(s):
     return s
 
 
+def trims(s):
+    if len(s) !=0:
+        res=''
+        for chr in s:
+            if chr!=' ':
+                res=res+chr
+    return res
+
+
 
 
 print(trim("    Hello,begin00"))
 
 print(trim('Hello ,end  !     '))
+
+print(trims("sdfsdf  uohoenx  我活 ！！！     "))
